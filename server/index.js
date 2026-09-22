e0784811ab3e35fff5bf8bafc350f4055243f508
sherylOchieng <sadhiambo998@gmail.com>
2026-09-22 16:00:55 +0300

    feat: mount Daraja callback route

--- server/index.js
+++ server/index.js
@@ -182,7 +182,9 @@ const telegram = require("./services/telegram.service");
 const env = require("./config/env");
 
 app.use("/telegram", require("./routes/telegram.routes"));
-app.use("/paystack", require("./routes/paystack.routes")); // Paystack webhook route
+app.use("/paystack", require("./routes/paystack.routes"));
+app.use("/daraja", require("./routes/daraja.routes")); // Daraja STK push callback
+
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
